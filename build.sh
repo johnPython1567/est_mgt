7a64cea2a69771fa1d2304e9fcbd6b63b8e9dbaf
John Python <johnfestus140@gmail.com>
2026-08-25 17:24:01 +0100

    Remove temporary migration diagnostic

--- build.sh
+++ build.sh
@@ -28,8 +28,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py showmigrations pages
-
-python manage.py migrate
-
 python manage.py createsuperuser --noinput || true
